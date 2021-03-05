96a13dc3c7a106832e6a4296598afbbe82e00d5d
Patrick Porschen <pp@naymspace.de>
2021-03-05 22:09:01 +0100

    Add types

--- PhotoGroove/src/PhotoGroove.elm
+++ PhotoGroove/src/PhotoGroove.elm
@@ -1,13 +1,25 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
+import Html exposing (Html, div, h1, img, text, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Browser
+import Array exposing (Array)
 
+type alias Model = 
+    {photos : List Photo, selectedUrl : String}
+
+type alias Msg =
+    {description : String, data : String}
+
+type alias Photo = 
+    {url : String}
+
+urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
@@ -23,6 +35,7 @@ view model =
             []
         ]
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img 
         [ src (urlPrefix ++ thumb.url) 
@@ -31,7 +44,7 @@ viewThumbnail selectedUrl thumb =
         ] 
         []
     
-
+initialModel : Model
 initialModel =
     { photos = 
         [ { url = "1.jpeg" }
@@ -41,9 +54,16 @@ initialModel =
     , selectedUrl = "1.jpeg"
     }
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+update : Msg -> Model -> Model
 update msg model =
     if msg.description == "ClickedPhoto" then
         { model | selectedUrl = msg.data }
+    else if msg.description == "ClickedSurpriseMe" then
+        { model | selectedUrl = "2.jpeg" }
     else
         model
         
